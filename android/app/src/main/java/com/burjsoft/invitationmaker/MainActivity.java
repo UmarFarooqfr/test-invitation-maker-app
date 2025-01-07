9763e0a6582714effb7bc3dfb7e0ca9e0fe5aa3f
Umar Farooq <131238639+UmarFarooqfr@users.noreply.github.com>
2025-01-07 15:22:25 +0500

    init

---
+++ android/app/src/main/java/com/burjsoft/invitationmaker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.burjsoft.invitationmaker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.burjsoft.invitationmaker',
+  appName: 'InvitationMaker',
+  webDir: 'dist/invitation-fe/browser'
+};
+
+export default config;
